316c49a114f466e85334e4157a6e058c73b894df
Carlos Steven Lozano <167788548+carlosste@users.noreply.github.com>
2025-02-10 13:44:04 +0000

    primera clase

---
+++ Estructura_De_Datos.py
@@ -0,0 +1,12 @@
+
+numeros = [4]
+
+
+int(print("Ingrese los numeros que desea ingresar en la lista. Limite 5 " , " Solo numeros Enteros"))
+
+for x in range(4):
+    numero = int(input( {x+1}))
+    numero.append(numero)
+
+
+print("Los numeros que ingreso son: ", numeros )
